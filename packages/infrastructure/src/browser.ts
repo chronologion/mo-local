3980808281dfed76bf04e0b4c82c03fdbfe644f6
Chronologion <chronologion@proton.me>
2025-12-30 02:17:07 +0200

    chore(infra): remove LiveStore runtime

--- packages/infrastructure/src/browser.ts
+++
@@ -1,9 +0,0 @@
-// Browser-focused exports only; avoid re-exporting Node/wa-sqlite or non-browser schemas.
-export * from './crypto/IndexedDBKeyStore';
-export * from './crypto/WebCryptoService';
-export * from './browser/LiveStoreEventStore';
-export * from './browser/sleep';
-export * from './browser/worker';
-export * from './goals';
-export * from './projects';
-export { tables } from './livestore/schema';
